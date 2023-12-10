a5fc637e7ad2e7b29b2693883357fffbab3fd774
Lucas <lucasemanuellq@gmail.com>
2023-12-10 15:53:58 -0300

    adicionado outro exemplo de gramatica

--- pln.pl
+++ pln.pl
@@ -4,3 +4,13 @@ predicado--> verbo, artigo, substantivo.
 artigo-->[o].
 substantivo-->[gato]|[rato].
 verbo-->[cacou].
+
+/* frase --> sujeito, predicado.
+sujeito --> artigo(G), substantivo(G).
+predicado --> verbo, artigo(G), substantivo(G).
+artigo(m) --> [o].
+artigo(f) --> [a].
+substantivo(m) --> [gato] | [rato].
+substantivo(f) --> [gata] | [rata].
+verbo --> [cacou].
+*/ 
